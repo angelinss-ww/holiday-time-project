61d7f48107f2900850139b680686f58c4cc7e65a
Angelina Oleksandrovich <pi59_oam@student.ztu.edu.ua>
2025-03-10 00:57:20 +0200

    create react app and create navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./img/logo.jpg";
+import globus from "./img/globus.svg"
+import "./styles/style.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="nav">
+        <div className="nav-logo">
+          <img src={logo} alt="logotype" className="logo"/></div>
+        <div className="container">
+          <div><a href="#" className="nav-main">Головна</a></div>
+          <div><a href="#" className="nav-aboutus">Про нас</a></div>
+          <div><a href="#" className="nav-book">Забронювати</a></div>
+          <div><a href="#" className="nav-contacts">Контакти</a></div>
+          <div><a href="#" className="nav-personal-accaunt">Особистий кабінет</a></div>
+          <div><img src={globus} alt="globus" className="globus" /></div>
+          <div><a href="#" className="english-version">En</a></div>
+        </div>
+      
+        
+      </nav>
+      <header className="header"></header>
+      <main className="main"></main>
+      <footer className="footer"></footer>
     </div>
   );
 }

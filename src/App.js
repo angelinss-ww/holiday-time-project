fd72deaf7c77e2f3484b72a232ce1f1228b782b3
Angelina Oleksandrovich <pi59_oam@student.ztu.edu.ua>
2025-03-16 23:41:55 +0200

    making the main page.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from "./img/logo.jpg";
 import globus from "./img/globus.svg"
+import holydaytime from "./img/holiday-time.svg"
+import carpatianhouse from "./img/carpatiam-house.svg"
 import "./styles/style.css"
 
 function App() {
@@ -19,11 +21,24 @@ function App() {
           <div><img src={globus} alt="globus" className="globus" /></div>
           <div><a href="#" className="english-version">En</a></div>
         </div>
-      
-        
       </nav>
       </header>
-      <main className="main"></main>
+      <main className="main">
+      <div className = "main-holiday-time">
+        <img src={holydaytime} alt="holydaytime" className="holydaytime"/> 
+        <h1>Holiday Time</h1>
+        <h2>Автентичний відпочинок серед українського колориту!</h2>
+      </div>
+      <div className= "main-slider">
+        <img src={carpatianhouse} alt="carpatianhouse" className="carpatianhouse"/>
+        <div className="slider-text"><span>Полонини Карпат, у селі Орів посеред гір розташувався затишний куточок для незабутніх вражень. <br/>
+        Справжні українські гори, власноручне сироваріння на полонині, водоспади та вікові дерева чекають на Вас.</span></div>
+        <a href="#" className="review">Переглянути &rarr;</a> 
+      </div>
+
+
+
+      </main>
       <footer className="footer"></footer>
       </body>
     </div>
